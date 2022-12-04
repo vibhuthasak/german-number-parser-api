e585e063c4e3fd6fdbeb368a67002f757b53bf2f
Vibhutha Kumarage <vibhutha.kumarage@dialog.lk>
2022-12-04 09:03:09 +0530

    basic AWS configs added

---
+++ lib/utility.py
@@ -0,0 +1,34 @@
+import boto3
+import logging
+
+logger = logging.getLogger(__name__)
+
+sqs_client = boto3.client("sqs")
+s3_client = boto3.client("s3")
+
+# Publish messege to SQS
+def publishToProcessQueue(message):
+    try:
+        response = sqs_client.send_message(
+            QueueUrl="https://sqs.ap-southeast-1.amazonaws.com/307676294310/numbers-to-process",
+            MessageBody=message,
+        )
+    except Exception as error:
+        logger.exception("Save failed")
+        return {"error": True, "description": str(error)}
+    else:
+        return {"error": False, "description": response}
+
+
+def saveFileOnS3(file, key):
+    try:
+        response = s3_client.upload_file(
+            Filename=file,
+            Bucket="numberfiles-german-numbers",
+            Key=key,
+        )
+    except Exception as error:
+        logger.exception("Save failed")
+        return {"error": True, "description": str(error)}
+    else:
+        return {"error": False, "description": response}
