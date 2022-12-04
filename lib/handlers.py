f5548e889083a274cadf01cd5b56b8d958e68f20
Vibhutha Kumarage <vibhutha.kumarage@dialog.lk>
2022-12-04 14:46:07 +0530

    processFile handler added

--- lib/handlers.py
+++ lib/handlers.py
@@ -1,4 +1,8 @@
 from lib import db, utility
+from werkzeug.utils import secure_filename
+import uuid
+
+OBJ_URL = "https://numberfiles-german-numbers.s3.ap-southeast-1.amazonaws.com/"
 
 
 def getAllTask(taskId):
@@ -19,3 +23,36 @@ def deleteTask(taskId):
         raise Exception(task["description"])
     else:
         return task["description"]["taskId"]
+
+
+def processFile(file):
+    try:
+        # Generate Filename, this will use as the key
+        fileKey = str(uuid.uuid1().hex)
+
+        # Save on system location
+        savedName = f"C:\\Users\Vibhutha\\Desktop\\NavVis-Code-Challenge-Cloud\\german-number-parser\\data\{secure_filename(fileKey)}.txt"
+        file.save(savedName)
+
+        # Save task on DB
+        dbResponse = db.createInitalTask()
+        if not dbResponse["error"]:
+            generatedTaskId = dbResponse["description"]["taskId"]
+            # Save on S3
+            s3Response = utility.saveFileOnS3(savedName, fileKey)
+            if not s3Response["error"]:
+                s3FileLocationName = OBJ_URL + fileKey + ".txt"
+                # Send event to SQS
+                event = {"taskId": generatedTaskId, "processFile": s3FileLocationName}
+
+                sqsResponse = utility.publishToProcessQueue(event)
+                if not sqsResponse["error"]:
+                    return {"error": False, "description": {"taskId": generatedTaskId}}
+                else:
+                    raise Exception(s3Response["description"])
+            else:
+                raise Exception(s3Response["description"])
+        else:
+            raise Exception(s3Response["description"])
+    except Exception as e:
+        return {"error": True, "description": str(e)}
