9c21063dd1abc09ec5e366901ca1edba7d8ed384
Vibhutha Kumarage <vibhutha.kumarage@dialog.lk>
2022-12-05 14:14:34 +0530

    removed mysql

--- lib/db.py
+++ lib/db.py
@@ -1,39 +1,44 @@
 import datetime
-
+from pymongo import MongoClient
 import logging
+import sys
 
 logger = logging.getLogger(__name__)
 
-"mongodb://master1:EnybGU522#xLZw@docdb-2022-12-05-07-41-33.cluster-cnpeiwumekol.ap-southeast-1.docdb.amazonaws.com:27017/?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+client = MongoClient(
+    "mongodb://master1:EnybGU522#xLZw@localhost:27017/?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false",
+    connect=False,
+)
 
 
 def createInitalTask():
     try:
-        return {"error": False, "description": {"taskId": task.task_id}}
+        client.externaldb.tasks.insert_one({"hello": "Amazon DocumentDB"})
+        return {"error": False, "description": {"taskId": 1}}
     except Exception as error:
         logger.exception("Save failed")
         return {"error": True, "description": str(error)}
 
 
-def getAllTasks():
-    try:
-        return {"error": False, "description": {"taskId": return_list}}
-    except Exception as error:
-        logger.exception("Select failed")
-        return {"error": True, "description": str(error)}
+# def getAllTasks():
+#     try:
+#         return {"error": False, "description": {"taskId": return_list}}
+#     except Exception as error:
+#         logger.exception("Select failed")
+#         return {"error": True, "description": str(error)}
 
 
-def getTask(taskId):
-    try:
-        return {"error": False, "description": {"taskId": task.as_dict()}}
-    except Exception as error:
-        logger.exception("Select failed")
-        return {"error": True, "description": str(error)}
+# def getTask(taskId):
+#     try:
+#         return {"error": False, "description": {"taskId": task.as_dict()}}
+#     except Exception as error:
+#         logger.exception("Select failed")
+#         return {"error": True, "description": str(error)}
 
 
-def deleteTask(taskId):
-    try:
-        return {"error": False, "description": {"taskId": task.task_id}}
-    except Exception as error:
-        logger.exception("Select failed")
-        return {"error": True, "description": str(error)}
+# def deleteTask(taskId):
+#     try:
+#         return {"error": False, "description": {"taskId": task.task_id}}
+#     except Exception as error:
+#         logger.exception("Select failed")
+#         return {"error": True, "description": str(error)}
